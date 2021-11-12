b7a2a6b75ba5652ce51a6c0f74e1de728ad9b7e3
27ishita <ishitadhar_d@srmuniv.edu.in>
2021-11-12 17:55:26 +0530

    drag and drop added

--- src/app/displaytask/displaytask.component.ts
+++ src/app/displaytask/displaytask.component.ts
@@ -1,4 +1,8 @@
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TaskService } from '../task.service';
@@ -7,94 +11,65 @@ import { Tasks } from '../tasks';
 @Component({
   selector: 'app-displaytask',
   templateUrl: './displaytask.component.html',
-  styleUrls: ['./displaytask.component.css']
+  styleUrls: ['./displaytask.component.css'],
 })
 export class DisplaytaskComponent implements OnInit {
-
-  constructor(private service: TaskService , private route : ActivatedRoute) { }
-  toDo : Tasks[] = [];
-  doing : Tasks[] = [];
-  done :Tasks[] = [];
-  task : Tasks = new Tasks();
- // id : number = 0;
-
+  constructor(private service: TaskService, private route: ActivatedRoute) {}
+  toDo: Tasks[] = [];
+  doing: Tasks[] = [];
+  done: Tasks[] = [];
+  task: Tasks = new Tasks();
 
   ngOnInit(): void {
-  this.displayAllProjects()
-   //this.getKanban();
+    this.displayAllProjects();
   }
 
-  displayAllProjects()
-  {
-    this.service.getAllTasks().subscribe(data=>{
-      data.forEach(t=>{
-        this.toDo.push(t);
-      })
-    })
+  displayAllProjects() {
+    this.service.getAllTasks().subscribe((data) => {
+      data.forEach((t) => {
+        if (t.status === 'todo') {
+          this.toDo.push(t);
+        } else if (t.status === 'doing') {
+          this.doing.push(t);
+        } else {
+          this.done.push(t);
+        }
+      });
+    });
   }
+
   drop(event: CdkDragDrop<Tasks[]>) {
-   if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
     } else {
-     // this.updateStatusAfterDrag(event);
-     transferArrayItem(event.previousContainer.data,
-                     event.container.data,
-                     event.previousIndex,
-                      event.currentIndex);
-    }
-  }
+      const data = event.previousContainer.data[event.previousIndex];
 
- /* private getKanban():void {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
 
-    const id = this.route.snapshot.params['id'];
+      switch (event.container.id) {
+        case 'todolist':
+          data.status = 'todo';
+          break;
 
-    this.service.getTaskbyid(id).subscribe((response)=>{
-      this.task = response;
-      this.getTaskByStatus(response)
-    })
+        case 'donelist':
+          data.status = 'done';
+          break;
 
-    
-  }
-
-
- private getTaskByStatus(task:Tasks):void{
-
-   if(task.status='todo')
-   this.toDo.push(task);
-   else if(task.status='doing')
-   this.doing.push(task);
-   else
-  
-   this.done.push(task);
-  }
-/*
-  private updateTaskStatus(task:Tasks,containerId:string)
-  {
-    if (containerId === 'todolist'){
-      task.status = 'todo'
-    } else if (containerId === 'doinglist'){
-      task.status = 'doing'
-    } else {
-      task.status = 'done'
-    }
-
-    this.service.updateTask(task,task.project_id).subscribe();
-  }
-
-  private updateStatusAfterDrag(event: CdkDragDrop<Tasks[]>):void{
-    let taskid:number = parseInt(event.item.element.nativeElement.id);
-    let containerId = event.container.id;
-    console.log(taskid);
-
-
-    this.service.getTaskbyid(taskid).subscribe(
-      response => {
-        this.updateTaskStatus(response,containerId);
-        console.log(taskid)
+        case 'doinglist':
+          data.status = 'doing';
+          break;
       }
-    );
+
+      this.service.updateTask(data, data.project_id).subscribe(console.log);
+    }
   }
-
-*/
-
 }
